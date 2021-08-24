be34e44c910a6452337c12b795ee22e7f258ba07
HeoJun <tntntnth@naver.com>
2021-08-25 02:01:00 +0900

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [ testStr, setTestStr ] = useState('');
+
+  // 변수 초기화
+  function callback(str) {
+    setTestStr(str);
+  }
+
+  // 첫 번째 렌더링을 마친 후 실행
+  useEffect(
+      () => {
+        axios({
+            url: '/user',
+            method: 'GET'
+        }).then((res) => {
+            callback(res.data);
+        })
+      }, []
+  );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <header className="App-header">
+        {testStr}
+    </header>
+</div>
   );
 }
 
