1fbd7734b42b48a5563c5d4e24f5bcbdd4df0d68
HeoJun <tntntnth@naver.com>
2021-08-26 00:33:15 +0900

    amplify commit 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react'
+import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
 
